37ba82f273b33c3703c288996af7500ff6b2c81e
Ariza97 <arizamtz2303@gmail.com>
2018-10-29 21:55:12 -0600

    Proyecto Tres terminado y funcionando

--- Practica 13 05_Octubre_2018/Cliente_Servidor(Primos)/Primos.cpp
+++ Practica 13 05_Octubre_2018/Cliente_Servidor(Primos)/Primos.cpp
@@ -11,47 +11,55 @@ int main(int argc, char const *argv[])
     int rodrigo =0, aidee =0;
 
     unsigned int num[3];
-    char ipRod[] = "10.100.66.153";
-    char ipAidee[] = "10.100.74.158";
-
+    char ipRod[] = "10.100.76.215";
+    char ipAidee[] = "10.100.74.178";
+    char ipErick[] = "10.100.73.194";
     //Rodrigo
     SocketDatagrama socket;
     num[0] = numeroPrimo;
     num[1] = 2;
-    num[2] = numeroPrimo/2; 
-    int *resRod; //Debe recibir O o 1    
+    num[2] = 3*(numeroPrimo/7); 
+    int *resRod; //Debe recibir O no es primo o 1 si es primp    
     PaqueteDatagrama paqRod((char *)num, 3*sizeof(unsigned int),ipRod,7200);
     socket.envia(paqRod);
-
-
    //Aidee
     num[0] = numeroPrimo;
-    num[1] = (numeroPrimo/2)+1;
-    num[2] = numeroPrimo-1;
+    num[1] = (3*(numeroPrimo/7))+1;
+    num[2] = 4*(numeroPrimo/6);
     int *resAidee; //Debe recibir O o 1
-    PaqueteDatagrama paqAidee((char *)num,3 * sizeof(int),ipAidee,7200);
+    PaqueteDatagrama paqAidee((char *)num, 3*sizeof(unsigned int),ipAidee,7200);
     socket.envia(paqAidee);
+   //Erick
+    num[0] = numeroPrimo;
+    num[1] = (4*(numeroPrimo/7))+1;
+    num[2] = numeroPrimo-1;
+    int *resErick; //Debe recibir O o 1
+    PaqueteDatagrama paqErick((char *)num, 3*sizeof(unsigned int),ipErick,7200);
+    socket.envia(paqErick);
 
 
     //Primera Respuesta
     PaqueteDatagrama paqRes1(sizeof(int));
     socket.recibe(paqRes1);
     resRod = (int *)paqRes1.obtieneDatos();
-    std::cout << "Respuesta Rod: " << *resRod << std::endl;
+    std::cout << "Respuesta 1: " << *resRod << std::endl;
     //Segunda Respuesta
     PaqueteDatagrama paqRes2(sizeof(int));
     socket.recibe(paqRes2);
     resAidee = (int *)paqRes2.obtieneDatos();
-    std::cout << "Respuesta Aidee: " << *resAidee << std::endl;
+    std::cout << "Respuesta 2: " << *resAidee << std::endl;
+    //Tercera Respuesta
+    PaqueteDatagrama paqRes3(sizeof(int));
+    socket.recibe(paqRes3);
+    resErick = (int *)paqRes3.obtieneDatos();
+    std::cout << "Respuesta 3: " << *resErick << std::endl;
 
 
-
-    if(resRod == 0 && resAidee == 0){
-        std::cout << "No es primo " << *resAidee << std::endl;
+    if ((*resRod == 1) && (*resErick == 1) && (*resAidee == 1)){
+        std::cout << "\n\nEs primo " << std::endl;
     }
     else{
-        std::cout << "Es primo " << *resAidee << std::endl;
+        std::cout << "\n\nNo es primo " << std::endl;
     }
-
     return 0;
 }
